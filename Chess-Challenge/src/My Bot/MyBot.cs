f6b34fe132cfa998572d1623f102e0ec743962e5
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-07-27 15:30:17 -0400

    Added a move Tracker

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,14 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        for (int i=0; i<moves.Length;i++){
+            Console.WriteLine(moves);
+        }
         return moves[0];
     }
 }
