f791c0deee9a9f27ef9fd95ba6356dce5e6e77f6
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-08-27 11:28:02 -0400

    I really dont want to work

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,6 +6,7 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        Console.WriteLine(timer.MillisecondsRemaining);
         for (int i=0; i<moves.Length;i++){
             Console.WriteLine(moves[i]);
             Console.WriteLine(moves[i].MovePieceType);
