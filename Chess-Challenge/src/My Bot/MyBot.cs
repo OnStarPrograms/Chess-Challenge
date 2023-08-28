8d32bf0f2bb4c1f1e2a08ca4818d60b3eb04723d
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-08-28 15:10:24 -0400

    Started working on chess bot
    Need to add point distributions

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,12 +5,37 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        int[] Points = [1, 3, 3, 4, 8 ];
+        String[] Peices = ["Pawn", "Knight", "Bishop", "Rook", "Queen"];
         Move[] moves = board.GetLegalMoves();
         Console.WriteLine(timer.MillisecondsRemaining);
-        for (int i=0; i<moves.Length;i++){
+        for (int i = 0; i < moves.Length; i++)
+        {
             Console.WriteLine(moves[i]);
             Console.WriteLine(moves[i].MovePieceType);
         }
-        return moves[0];
+
+
+        int Recurse(int Depth, int Points)
+        {
+            Move[] moves = board.GetLegalMoves();
+            for (int j = 0 ; j < moves.Length; j++)
+            {
+                if (Depth <= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Depth);
+                    return Recurse(Depth - 1, Points+1);
+                }
+            }
+            return Points;
+        }
+
+        int Rec = Recurse(3, 3);
+        Console.WriteLine(Rec);
+        return moves[Rec];
     }
 }
