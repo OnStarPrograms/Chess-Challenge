94b2a71c93327d96d255335fb88b18e6b29c4937
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-09-04 21:40:36 -0400

    damn that was difficult
    Added a Point Management System

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,6 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Drawing;
 
 /*
  * Time Tracker that will stop the recursion if run out of time
@@ -11,6 +12,24 @@ using System;
  * 
  * 
  */
+class Alloc
+{
+    public int[] MyPointts;
+    public Alloc(int Moves)
+    {
+        MyPointts = new int[Moves];
+    }
+
+    public void AddPoint(int P, int Accesor)
+    {
+        MyPointts[Accesor] += P;
+    }
+    public int returnPoint(int Accessor)
+    {
+        return MyPointts[Accessor];
+    }
+}
+
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
@@ -19,6 +38,7 @@ public class MyBot : IChessBot
         String[] Peices = ["Pawn", "Knight", "Bishop", "Rook", "Queen"];
         Move[] moves = board.GetLegalMoves();
         int[] depth =  new int[moves.Length];
+        Alloc MyPoints = new Alloc(moves.Length);
         //////////////////////////////////////////////////////////
 
         for (int i = 0; i < moves.Length; i++)
@@ -29,32 +49,32 @@ public class MyBot : IChessBot
         }
         /////////////////////////////////////////////////////
 
-        int Recurse(int Depth, int Points)
+        void Recurse(int Depth, int Points, int Ass)
         {
             Move[] moves2 = board.GetLegalMoves();
             int RecursionDepth = Depth;
             for (int j = 0 ; j < moves2.Length; j++)
             {
-                if (Depth <= 0)
+                if (Depth <= 0 || timer.MillisecondsRemaining<10000)
                 {
                     break;
                 }
                 else
                 {
-                    Console.WriteLine(Depth);
-                    Console.WriteLine("move choice" + j.ToString());
-                    
-                    Points += Recurse(RecursionDepth - 1, Points*-1);
+                    //Console.WriteLine(Depth);
+                    //Console.WriteLine("move choice" + j.ToString());
+                    int TotalPoint = 1;
+                    MyPoints.AddPoint(TotalPoint, Ass);
+                    Recurse(RecursionDepth - 1, Points*-1, Ass);
                 }
             }
-            return Points;
         }
         /////////////////////////////////////////////////////////
-        
         for (int i = 0; i < moves.Length; i++)
         {
-            int Rec = Recurse(depth[i], 3);
-            Console.WriteLine(Math.Abs(Rec));
+            Recurse(depth[i], 1, i);
+            Console.WriteLine(MyPoints.returnPoint(i));
+            Console.Write(moves[i].MovePieceType);
         }
         return moves[0];
     }
